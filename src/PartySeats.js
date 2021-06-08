5ac40e4f5ba3cb49125be30fbeddaa76d8deaa7f
benfanderson <benfanderson81@gmail.com>
2021-06-08 20:35:11 +0800

    Split styles into sass partials

--- src/PartySeats.js
+++ src/PartySeats.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import './styles.scss';
+import './partySeatsStyle.scss';
 
 const parties = (nationalsCandidate) => {
   if (nationalsCandidate === true) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './_base.scss';
 
 import App from './App';
 
